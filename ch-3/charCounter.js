481001f47c8da2a2cd99d20daa1b3bf2675fd223
ibtisam <iuhmirza@gmail.com>
2022-08-14 12:10:52 +0100

    Complete 'Bean Counter'

---
+++ ch-3/charCounter.js
@@ -0,0 +1,16 @@
+function countBs(string) {
+    let counter = 0;
+    for(let i = 0; i < string.length; i++) {
+      if (string[i] == 'B') counter++;
+    } return counter;
+  }
+    
+  function countChar(string, char) {
+    let counter = 0;
+    for(let i = 0; i < string.length; i++) {
+      if (string[i] == char) counter++;
+    } return counter;
+  }
+  
+  console.log(countBs("BBC"));
+  console.log(countChar("kakkerlak", "k"));
